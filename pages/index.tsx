d0789f1f120af7f09fe0345e44455052e54f882b
Francisco Gonzalez <frangc0206@gmail.com>
2022-12-03 19:31:13 -0300

    Cleaned up main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import Head from "next/head";
-import Image from "next/image";
-import MapComponent from "../components/Map";
-import Siedbar from "../components/Siedbar";
-import styles from "../styles/Home.module.css";
-import { useState } from "react";
+import MapComponent from "../components/Map/Map";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 export default function Home() {
-  const [mapFiles, setMapFiles] = useState([]);
   return (
-    <section className="flex flex-row w-full h-screen overflow-hidden">
-      {/* <Siedbar mapFiles={mapFiles} /> */}
-      <MapComponent mapFiles={mapFiles} />
-    </section>
+    <>
+    <Head>
+      <title>KML Visualizer</title>
+    </Head>
+      <section className="flex flex-row w-full h-screen overflow-hidden">
+        <Sidebar />
+        <MapComponent />
+      </section>
+    </>
   );
 }
