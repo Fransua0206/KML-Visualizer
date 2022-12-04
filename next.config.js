f2236dce9ba6fd4a2b632cdb20e2d2038ff10b07
Francisco Gonzalez <frangc0206@gmail.com>
2022-12-03 21:18:13 -0300

    Updated secrets

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
-}
+  env: {
+    apiURL: process.env.apiURL,
+    apiServiceKey: process.env.apiServiceKey,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
