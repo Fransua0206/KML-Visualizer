977c712fd425db72707838a9bbc7d5b08a8163f4
Francisco Gonzalez <frangc0206@gmail.com>
2022-12-06 18:56:28 -0300

    Adjusted function to meet color selector needs

--- tools/MapTools.ts
+++ tools/MapTools.ts
@@ -4,56 +4,66 @@ import Fill from "ol/style/Fill";
 import Stroke from "ol/style/Stroke";
 import Style from "ol/style/Style";
 
-const fillOpacity = "0.2"
+const fillOpacity = "0.2";
 
-export const centerSyle = (feature: any) => {
-  let style = new Style({
-    stroke: new Stroke({
-      color: "rgb(123, 154, 175)",
-      width: 1,
-    }),
-    fill: new Fill({
-      color: `rgba(123, 154, 175, ${fillOpacity})`,
-    }),
-  });
-  return [style];
-};
+export const getStyle = (feature: any) => {
+  const selectedColor = window.localStorage.getItem("shape-color");
 
-export const towerStyle = (feature: any) => {
-  let style = new Style({
-    stroke: new Stroke({
-      color: "rgb(255, 87, 81)",
-      width: 1,
-    }),
-    fill: new Fill({
-      color: `rgba(255, 87, 81, ${fillOpacity})`,
-    }),
-  });
-  return [style];
-};
-
-export const departureStyle = (feature: any) => {
-  let style = new Style({
-    stroke: new Stroke({
-      color: "rgb(255, 204, 255)",
-      width: 1,
-    }),
-    fill: new Fill({
-      color: `rgba(255, 204, 255, ${fillOpacity})`,
-    }),
-  });
-  return [style];
-};
-
-export const approachStyle = (feature: any) => {
-  let style = new Style({
-    stroke: new Stroke({
-      color: "rgb(112, 165, 236)",
-      width: 1,
-    }),
-    fill: new Fill({
-      color: `rgba(112, 165, 236, ${fillOpacity})`,
-    }),
-  });
-  return [style];
+  switch (selectedColor) {
+    case "RED":
+      let redStyle = new Style({
+        stroke: new Stroke({
+          color: "rgb(255, 87, 81)",
+          width: 1,
+        }),
+        fill: new Fill({
+          color: `rgba(255, 87, 81, ${fillOpacity})`,
+        }),
+      });
+      return [redStyle];
+    case "BLUE":
+      let blueStyle = new Style({
+        stroke: new Stroke({
+          color: "rgb(112, 165, 236)",
+          width: 1,
+        }),
+        fill: new Fill({
+          color: `rgba(112, 165, 236, ${fillOpacity})`,
+        }),
+      });
+      return [blueStyle];
+    case "PINK":
+      let pinkStyle = new Style({
+        stroke: new Stroke({
+          color: "rgb(255, 204, 255)",
+          width: 1,
+        }),
+        fill: new Fill({
+          color: `rgba(255, 204, 255, ${fillOpacity})`,
+        }),
+      });
+      return [pinkStyle];
+    case "GRAY":
+      let grayStyle = new Style({
+        stroke: new Stroke({
+          color: "rgb(123, 154, 175)",
+          width: 1,
+        }),
+        fill: new Fill({
+          color: `rgba(123, 154, 175, ${fillOpacity})`,
+        }),
+      });
+      return [grayStyle];
+    default:
+      let defaultStyle = new Style({
+        stroke: new Stroke({
+          color: "rgb(255, 204, 255)",
+          width: 1,
+        }),
+        fill: new Fill({
+          color: `rgba(255, 204, 255, ${fillOpacity})`,
+        }),
+      });
+      return [defaultStyle];
+  }
 };
