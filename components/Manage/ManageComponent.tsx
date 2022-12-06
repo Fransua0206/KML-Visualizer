a95ca141a5f3a832af47c5ef6f014292acdf48d1
Francisco Gonzalez <frangc0206@gmail.com>
2022-12-06 20:42:01 -0300

    Implemented map style selector

--- components/Manage/ManageComponent.tsx
+++ components/Manage/ManageComponent.tsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { BiTrash } from "react-icons/bi";
 
+interface SelectProps {
+  tag: string;
+  value: string;
+}
+
 const ManageComponent = () => {
   const supabaseClient = createClient(
     process.env.apiURL!,
@@ -10,7 +15,7 @@ const ManageComponent = () => {
   const [foundFiles, setFoundFiles] = useState<string[]>([]);
   const [ready, setReady] = useState(false);
 
-  const availableColors = [
+  const availableColors: SelectProps[] = [
     {
       tag: "Red",
       value: "RED",
@@ -21,12 +26,35 @@ const ManageComponent = () => {
     },
     {
       tag: "Pink",
-      value: "PINK"
+      value: "PINK",
     },
     {
       tag: "Grey",
-      value: "GREY"
-    }
+      value: "GREY",
+    },
+  ];
+
+  const availableMaps: SelectProps[] = [
+    {
+      tag: "Argenmap",
+      value:
+        "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png",
+    },
+    {
+      tag: "Argenmap (gris)",
+      value:
+        "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/mapabase_gris@EPSG%3A3857@png/{z}/{x}/{-y}.png",
+    },
+    {
+      tag: "Argenmap (topográfico)",
+      value:
+        "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/mapabase_topo@EPSG%3A3857@png/{z}/{x}/{-y}.png",
+    },
+    {
+      tag: "Argenmap (oscuro)",
+      value:
+        "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/argenmap_oscuro@EPSG%3A3857@png/{z}/{x}/{-y}.png",
+    },
   ];
 
   useEffect(() => {
@@ -45,8 +73,12 @@ const ManageComponent = () => {
     getData();
   }, []);
 
+  // Localstorage functions
   const setShapeColor = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    window.localStorage.setItem('shape-color', e.target.value)
+    window.localStorage.setItem("shape-color", e.target.value);
+  };
+  const setMapStyle = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    window.localStorage.setItem("map-style", e.target.value);
   };
   return (
     <>
@@ -64,11 +96,31 @@ const ManageComponent = () => {
         <div className="flex flex-col ml-2 mt-5 w-52 h-20 md:ml-10 md:mt-0">
           <h1 className="font-semibold text-2xl mb-2">Shape color selection</h1>
           <select onChange={setShapeColor} name="Colors">
-            <option value="none" selected disabled hidden>Select an option</option>
+            <option value="none" selected disabled hidden>
+              Select an option
+            </option>
             {availableColors.map((color, index) => {
               return (
                 <option key={index} value={color.value}>
                   {color.tag}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        {/* Map style selector */}
+        <div className="flex flex-col ml-2 mt-5 w-52 h-20 md:ml-10 md:mt-0">
+          <h1 className="font-semibold text-2xl mb-2">Map style selector</h1>
+          <select onChange={setMapStyle} name="Map">
+            {/* Placeholder */}
+            <option value="none" selected disabled hidden>
+              Select an option
+            </option>
+            {/* Options mapping */}
+            {availableMaps.map((map, index) => {
+              return (
+                <option key={index} value={map.value}>
+                  {map.tag}
                 </option>
               );
             })}
